7488ec6ceac5a9e8c428a716cff9e2301d6fb37d
sviam-byte <kudryavaya2020@gmail.com>
2025-08-27 22:57:13 +0300

    Update main.js

--- o2log/main.js
+++ o2log/main.js
@@ -1,6 +1,8 @@
-import React from 'https://esm.sh/react@18';
-import { createRoot } from 'https://esm.sh/react-dom@18/client';
-import * as Recharts from 'https://esm.sh/recharts@2';
+// o2log/main.js
+import React from 'https://esm.sh/react@18.2.0?dev';
+import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client?dev';
+// ВАЖНО: у Recharts добавляем ?bundle, чтобы подтянулись все d3-зависимости.
+import * as Recharts from 'https://esm.sh/recharts@2.10.4?bundle&target=es2018&dev';
 
 import O2LogApp from './O2LogApp.js';
 
