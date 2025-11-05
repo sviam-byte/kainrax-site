8a7ea1be3b32a561a234b1bd442886c1ebecc17f
sviam-byte <kudryavaya2020@gmail.com>
2025-11-05 16:20:21 +0300

    Update astro.config.mjs

--- canonar/astro.config.mjs
+++ canonar/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  base: '/canonar/',
+  base: '/',                // важное отличие от варианта с под-путём
+  site: 'https://canonar.<твой-домен>', // опционально, но полезно
   output: 'static'
 });
